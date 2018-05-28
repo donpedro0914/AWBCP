2c75ca073b9b93657386f32a87e9d8323d817453
Peter Ordonez <donpedro.ordonez@gmail.com>
2018-05-28 14:48:37 +0800

    fix homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,10 @@
 	<div class="content">
 		<div class="row js-masonry">
 			@foreach ($productList as $product)
-				<div class="col-md-3 col-xs-12 push-10 js-masonry-sizer">
+				<div class="col-md-2 col-xs-12 push-10 js-masonry-sizer">
 					<a class="block" href="/product/x/{{ $product->product_name }}">
 						<div class="block-content block-content-full">
-							{{ HTML::image('product/'. $product->product_name .'/'.$product->product_image, $product->product_name, array('class' => 'img-responsive')) }}
+							{{ HTML::image('product/'. $product->product_name .'/'.$product->product_image, $product->product_name, array('class' => 'img-thumbnail', 'style' => 'width:100%')) }}
 							<div class="block-content block-content-full">
 								<h2 class="h2 push-10">{{ $product->product_name }}</h2>
 							</div>
