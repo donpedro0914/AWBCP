5adbede272d133e20f080a770f9d3914593d11b0
Peter Ordonez <donpedro.ordonez@gmail.com>
2018-05-30 14:22:43 +0800

    fix login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -47,7 +47,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+                            <div class="col-md-8 col-md-offset-5">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
